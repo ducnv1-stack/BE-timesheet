52db9388197b30d340187f0e34b4cea5fd1994d5
ducnv1-stack <ducnv1@superbai.io>
2026-03-08 07:40:22 +0700

    fix build backend

--- prisma/migrations/20260308063000_add_avatar_and_order_images/migration.sql
+++ prisma/migrations/20260308063000_add_avatar_and_order_images/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE "Employee" ADD COLUMN "avatar_url" TEXT;
+ALTER TABLE "employees" ADD COLUMN "avatar_url" TEXT;
 
 -- AlterTable
-ALTER TABLE "Order" ADD COLUMN "images" TEXT[] DEFAULT ARRAY[]::TEXT[];
+ALTER TABLE "orders" ADD COLUMN "images" TEXT[] DEFAULT ARRAY[]::TEXT[];
