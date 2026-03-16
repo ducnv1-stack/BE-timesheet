168eb3593eb7fc4aabd6aa20bd76fb7c669cf389
ducnv1-stack <ducnv1@superbai.io>
2026-03-17 05:54:34 +0700

    del

--- test-api.ts
+++
@@ -1,6 +0,0 @@
-async function test() {
-    const res = await fetch('http://localhost:3001/dashboard?userId=d1e1882c-4638-40af-a9e3-a1288c1c4b8b'); // Use a known director ID or any ID that hits director
-    const data = await res.json();
-    console.log(JSON.stringify(data, null, 2));
-}
-test().catch(console.error);
